02dbf69b08c33fa91a01f3f455d1777e56ad6dde
Jitang Zheng <jitang.zheng@gmail.com>
2016-11-23 08:03:54 -0800

    notices

--- app/controllers/ops/notices_controller.rb
+++ app/controllers/ops/notices_controller.rb
@@ -2,7 +2,45 @@ require_dependency "ops/application_controller"
 
 module Ops
   class NoticesController < ApplicationController
-    def index
+    def new
+      @notice = Notice.new
+    end
+
+    def create
+      @notice = Notice.new(notice_params)
+      @notice.lang = I18n.locale
+
+      if @notice.save
+        redirect_to notices_path
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @notice = Notice.find(params[:id])
+    end
+
+    def update
+      @notice = Notice.find(params[:id])
+      if @notice.update(notice_params)
+        redirect_to notices_path
+      else
+        render 'edit'
+      end
+
+    end
+
+    def destroy
+      n = Notice.find(params[:id])
+      n.destroy
+
+      redirect_to notices_path
+    end
+
+    private
+    def notice_params
+      params.require(:notice).permit(:body)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 require 'sidekiq/web'
 
 Ops::Engine.routes.draw do
-  resources :notices
-  resources :leave_words, only:[:index, :destroy]
+  scope '/:locale' do
+    resources :notices, except: :show
+    resources :leave_words, only:[:index, :destroy]
 
-  %w(status engines).each {|e| get "site/#{e}"}
-  %w(info seo).each do |e|
-    get "site/#{e}"
-    post "site/#{e}"
+    %w(status engines).each {|e| get "site/#{e}"}
+    %w(info seo).each do |e|
+      get "site/#{e}"
+      post "site/#{e}"
+    end
   end
 
   authenticate :user, lambda { |u| u.is_admin? } do
